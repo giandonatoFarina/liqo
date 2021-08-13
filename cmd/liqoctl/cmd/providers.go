f014cd50e78aeb0c495941eff32f8641ec0890bd
Alessandro Olivero <aleoli96@gmail.com>
2021-08-13 09:54:10 +0000

    add liqoctl aks provider

--- cmd/liqoctl/cmd/providers.go
+++ cmd/liqoctl/cmd/providers.go
@@ -3,13 +3,15 @@ package cmd
 import (
 	flag "github.com/spf13/pflag"
 
+	"github.com/liqotech/liqo/pkg/liqoctl/install/aks"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/eks"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/kubeadm"
 )
 
-var providers = []string{"kubeadm", "eks"}
+var providers = []string{"kubeadm", "eks", "aks"}
 
 var providerInitFunc = map[string]func(*flag.FlagSet){
 	"kubeadm": kubeadm.GenerateFlags,
 	"eks":     eks.GenerateFlags,
+	"aks":     aks.GenerateFlags,
 }
--- pkg/liqoctl/install/utils.go
+++ pkg/liqoctl/install/utils.go
@@ -11,6 +11,7 @@ import (
 	"helm.sh/helm/v3/pkg/action"
 	"k8s.io/client-go/rest"
 
+	"github.com/liqotech/liqo/pkg/liqoctl/install/aks"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/eks"
 	helmutils "github.com/liqotech/liqo/pkg/liqoctl/install/helm"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/kubeadm"
@@ -24,6 +25,8 @@ func getProviderInstance(providerType string) provider.InstallProviderInterface 
 		return kubeadm.NewProvider()
 	case "eks":
 		return eks.NewProvider()
+	case "aks":
+		return aks.NewProvider()
 	default:
 		return nil
 	}
